a4852274bf5b762ffe5e55a5270c2e4ce86ad0ab
Nick-Yk <yakumov80@gmail.com>
2021-11-18 13:36:46 +0200

    add view

--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,64 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def viewdetails(request, lot_id):
+    current_lot = Lot.objects.get(pk=lot_id)
+    print(f"lotid={lot_id}")
+
+    if request.method == "POST" and "createbid" in request.POST and request.POST["bidprice"] != '':
+        #print(f"request={request.POST}")
+        lastbidprice = int(request.POST["bidprice"])
+        userid = request.POST["userid"]
+        biduser = User.objects.get(id=userid)
+        print(f"bidprice={lastbidprice}")
+        print(f"userid={userid}")
+        print(f"biduser={biduser}")
+        #oldprice = Lot.objects.get(pk=lot_id)
+        oldprice = Lot.objects.values_list('lot_price', flat=True).get(pk=lot_id)
+        print(f"oldprice={oldprice}")
+        if lastbidprice <= oldprice:
+            print("bid no")
+            print(f"corentlot={current_lot}")
+            return render(request, "auctions/viewdetails.html", {
+                 "message": "The bid must be more then oldprice !",
+                 "currentlot": current_lot,
+                 "currentbids": Bid.objects.all().filter(bid_lot_id=lot_id).order_by('-id'),
+                 "comments": Comment.objects.all().filter(comment_lot_id=lot_id).order_by('-id'),
+                  "allbid": Bid.objects.all()
+         })
+        else:
+            print("bid ok")
+            #обновить таблицу лот значення бид и прайс
+            newbid = Bid.objects.create(bid_user=biduser, bid_price=lastbidprice, bid_lot_id=lot_id)
+            newbid.save()
+            print(f"newbid={newbid}")
+            current_lot.lot_price = lastbidprice
+            current_lot.lot_bid = newbid
+            current_lot.save()
+            print(f"current_lotn={current_lot}")
+            return HttpResponseRedirect(reverse("viewdetails", args=(current_lot.id,)))
+    elif request.method == "POST" and "createcomment" in request.POST:
+        print("lot comment")
+        print(f"lotcomment={request.POST}")
+        userid = request.POST["userid"]
+        commentuser = User.objects.get(id=userid)
+        commenttext = request.POST["commenttext"]
+        commentdate = datetime.datetime.now()
+        print(f"comuser={commentuser}, comtext={commenttext}, comdat={commentdate}")
+        newcomment = Comment.objects.create(comment_user=commentuser, comment_text=commenttext, comment_date=commentdate, comment_lot_id=lot_id)
+        newcomment.save()
+        return HttpResponseRedirect(reverse("viewdetails", args=(current_lot.id,)))
+    elif request.method == "POST" and "closedlot" in request.POST:
+        print("lot closed")
+        current_lot.lot_status = False
+        current_lot.save()
+        return HttpResponseRedirect(reverse("viewdetails", args=(current_lot.id,)))
+    else:
+        return render(request, "auctions/viewdetails.html", {
+        "currentlot": current_lot,
+        "currentbids": Bid.objects.all().filter(bid_lot_id=lot_id).order_by('-id'),
+        "comments": Comment.objects.all().filter(comment_lot_id=lot_id).order_by('-id'),
+         "allbid": Bid.objects.all()
+    })
